e3da8a7f7c49b5910d13b62100a16df00775638a
davidmaxdmao <davidmax.dmao@gmail.com>
2019-10-28 20:43:41 -0300

    Add files via upload
    Atividade de revisão da segunda unidade

--- Program.cs
+++ Program.cs
@@ -1,122 +1,25 @@
 ﻿using System;
 
-namespace Aula_19_08_2019
+namespace RevisaoSegundaUnidade
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*  Questão 1
-             *  
-             *  Console.Write("informe um numero");
-              int num;
-              num = int.Parse(Console.ReadLine());
-              Console.WriteLine("numero: " + num);
-              */
+            Animais a = new Animais();
+            DonoAnimal d = new DonoAnimal();
+            a.animal = "tartaruga";
+            a.idade = 0;
+            a.nome = "simon";
+            a.Andar();
+            a.Comer();
+            a.Nome();
 
-            /*
-             * questao 2
-             * 
-            Console.Write("informe um numero");
-            int num;
-            num = int.Parse(Console.ReadLine());
-            Console.Write("informe outro numero");
-            int num2;
-            num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("numero 1: " + num);
-            Console.WriteLine("numero 2: " + num2);
-            */
+            d.NomeDono = "david";
+            d.NomeAnimal = a.nome;
+            d.Dono();
 
-            /* 
-             questao 3
 
-             Console.Write("informe um numero");
-             int num;
-             num = int.Parse(Console.ReadLine());
-
-             Console.WriteLine("Sucessor: " + (num + 1) + "antecessor: " + (num - 1));
-             */
-
-            /*
-              * questal 4
-              * 
-            string nome, endereco, telefone;
-            Console.WriteLine("informe o nome: ");
-            nome = Console.ReadLine();
-            Console.WriteLine("informe o endereco: ");
-            endereco = Console.ReadLine();
-            Console.WriteLine("INFORME O TELEFONE: ");
-            telefone = Console.ReadLine();
-            Console.WriteLine();
-            Console.WriteLine(nome);
-            Console.WriteLine(endereco);
-            Console.WriteLine(telefone);
-            */
-
-            /*
-             *questao 5 
-             * 
-            Console.Write("informe um numero ");
-            int num;
-            num = int.Parse(Console.ReadLine());
-            Console.Write("informe outro numero ");
-            int num2;
-            num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Soma: " + (num + num2));
-            */
-
-            /*
-             * questao 6
-             * 
-            Console.Write("informe um numero ");
-            float num;
-            num = float.Parse(Console.ReadLine());
-            Console.Write("informe outro numero ");
-            float num2;
-            num2 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Produto: " + (num * num2));
-            */
-
-            /*
-             * questao 7
-             * 
-            Console.Write("informe um numero ");
-            float num;
-            num = float.Parse(Console.ReadLine());
-            num = (num / 3);
-            Console.WriteLine("terça parte: " + num);
-            */
-
-            /*
-             * questao 8
-             * 
-            Console.Write("informe um numero ");
-            float num;
-            num = float.Parse(Console.ReadLine());
-            Console.Write("informe outro numero ");
-            float num2;
-            num2 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Média: " + ((num + num2) / 2));
-            */
-
-            /*
-             *questao 9 
-             * 
-            Console.Write("informe um numero ");
-            float num;
-            num = float.Parse(Console.ReadLine());
-            Console.Write("informe outro numero ");
-            float num2;
-            num2 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Dividendo: "+(num));
-            Console.WriteLine("Divisor: "+(num2));
-            Console.WriteLine("Quociente: "+ (num/num2));
-            Console.WriteLine("Resto: "+(num%num2));
-            */
         }
     }
 }
